4801e3f0bcbdb3e09d3ad860000fe9becd4f64d9
uchitm1 <uchitmahapatra30@gmail.com>
2021-07-11 10:36:26 +0530

    Removed unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -4,33 +4,19 @@ import Loader from "./components/Loader";
 import * as Routes from "./constants/routes";
 import UserContextProvider from "./contexts/user";
 
+const Landing = lazy(() => import("./pages/Landing"));
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
 const Register = lazy(() => import("./pages/Register"));
 
 function App() {
-	// const [loggedInUser, setLoggedInUser] = useState({});
-
-	// const currentUser = async () => {
-	// 	await apis
-	// 		.getLoggedInUser()
-	// 		.then((res) => {
-	// 			console.log("Logged in user - " + JSON.stringify(res.data.user));
-	// 			setLoggedInUser(res.data.user);
-	// 		})
-	// 		.catch((err) => console.error(err.response.data));
-	// };
-
-	// useEffect(() => {
-	// 	currentUser();
-	// }, []);
-
 	return (
 		<UserContextProvider>
 			<div className="App">
 				<Router>
 					<Suspense fallback={<Loader />}>
 						<Switch>
+							<Route exact path={Routes.DEFAULT} component={Landing} />
 							<Route exact path={Routes.HOME} component={Home} />
 							<Route exact path={Routes.LOGIN} component={Login} />
 							<Route exact path={Routes.REGISTER} component={Register} />
